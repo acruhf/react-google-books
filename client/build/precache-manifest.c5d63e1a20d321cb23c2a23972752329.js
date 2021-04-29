6f71ca295f0167b274c5116198adf6f88edb12e7
Austin Ruhf <austin.ruhf@gmail.com>
2021-04-28 22:00:08 -0400

    something new

---
+++ client/build/precache-manifest.c5d63e1a20d321cb23c2a23972752329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a523f43d47ad6b5807f55fe24c305d76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ee7dcd18d4fd0220e8",
+    "url": "/static/css/main.7f510bf3.chunk.css"
+  },
+  {
+    "revision": "522c6da5c261361c6d71",
+    "url": "/static/js/2.491aa3f2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.491aa3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ee7dcd18d4fd0220e8",
+    "url": "/static/js/main.37c1e998.chunk.js"
+  },
+  {
+    "revision": "226fd4a5de4b477d8cce",
+    "url": "/static/js/runtime-main.6ff082a3.js"
+  }
+]);
